25f08aae15cbfba8d9c9b2613961db865423b809
robin <robinnsheen@berkeley.edu>
2022-06-27 17:04:33 -0700

    started pt 2 cards api

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 "use strict";
 
 const NUMS_URL = "http://numbersapi.com/";
+const CARDS_URL = "http://deckofcardsapi.com/api/";
 
 async function getNumberFact() {
   const response = await axios.get("http://numbersapi.com/1,2,3,4?json");
-  console.log("response.data", response.data);
+  // console.log("response.data", response.data);
   const numFacts = response.data;
   for (let num in numFacts) {
     $("#num-facts").append(`<p>${numFacts[num]}</p>`);
@@ -20,7 +21,7 @@ async function getFactsForFavNum() {
   const resp4 = axios.get("http://numbersapi.com/42?json");
 
   let results = await Promise.allSettled([resp1, resp2, resp3, resp4]);
-  console.log(results);
+  // console.log(results);
   const numFacts = results.map(fact => fact.value.data.text)
 
   for (let num in numFacts) {
@@ -29,3 +30,19 @@ async function getFactsForFavNum() {
 }
 
 getFactsForFavNum();
+
+async function shuffleAndDraw() {
+  const resp = await axios({
+    baseURL: CARDS_URL,
+    url: "deck/new/draw/",
+    method: "GET",
+    params: {"count": 1}
+  });
+  const card = resp.data.cards[0];
+  const cardVal = card.value;
+  const cardSuit = card.suit;
+
+  console.log(`${cardVal} of ${cardSuit}`);
+}
+
+shuffleAndDraw();
